810b8ee23557be59d814ee2516b3bf825d654455
kosta <k8.solonovich@gmail.com>
2022-05-19 14:30:27 +0200

    modal form added

--- django_test/test_project/blog/views.py
+++ django_test/test_project/blog/views.py
@@ -1,5 +1,3 @@
-from contextlib import nullcontext
-from email.mime import image
 from django.shortcuts import redirect, render
 from django.utils import timezone
 
@@ -7,17 +5,11 @@ from .models import Post
 
 
 def index(request):
-    content = Post.objects.order_by('-pub_date')
-
-    return render(request, 'index.html', {'content': content})
-
-
-def new_post(request):
-    content = Post.objects.order_by('-pub_date')
+    context = Post.objects.order_by('-pub_date')
     if request.method == 'POST':
         text = request.POST['text']
-        get_image = request.FILES['image']
+        get_image = request.FILES.get('image')
         Post.objects.create(text=text, pub_date=timezone.now(), image=get_image)
         return redirect('/')
 
-    return render(request, 'new-post.html', {'content': content})
+    return render(request, 'index.html', {'context': context})
